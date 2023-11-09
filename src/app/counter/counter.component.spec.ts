5913a345fa9aae002340071d89cd8296ec320f25
Zhiping Shu <ZShu@slb.com>
2023-11-09 16:16:21 +0800

    feat: should increase number and decrease number when call onIncrease and onDecrease method

---
+++ src/app/counter/counter.component.spec.ts
@@ -0,0 +1,33 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CounterComponent } from './counter.component';
+
+describe('CounterComponent', () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CounterComponent]
+    });
+    fixture = TestBed.createComponent(CounterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should increase number when call onIncrease', () => {
+    component.number = 2
+    component.onIncrease()
+    expect(component.number).toEqual(3)
+  });
+
+  it('should decrease number when call onDecrease', () => {
+    component.number = 2
+    component.onDecrease()
+    expect(component.number).toEqual(1)
+  })
+});
