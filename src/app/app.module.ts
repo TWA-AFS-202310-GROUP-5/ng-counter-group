d50f9932475e46082c41d805950fc409f98529bf
Jinglin Wang <JWang317@slb.com>
2023-11-09 16:09:51 +0800

    refactor: rename component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterGroupComponent } from './counter-group/counter-group.component';
-import { CouterComponent } from './counter-group/couter/couter.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterGroupComponent,
-    CouterComponent
+    CounterComponent
   ],
   imports: [
     BrowserModule,
