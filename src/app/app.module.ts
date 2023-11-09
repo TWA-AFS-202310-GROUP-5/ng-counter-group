83ea659669722b3156e278d15a8b542024e2222f
Jinglin Wang <JWang317@slb.com>
2023-11-09 15:58:01 +0800

    feat: create empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterGroupComponent } from './counter-group/counter-group.component';
+import { CouterComponent } from './counter-group/couter/couter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterGroupComponent,
+    CouterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/counter-group/counter-group.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-group',
+  templateUrl: './counter-group.component.html',
+  styleUrls: ['./counter-group.component.css']
+})
+export class CounterGroupComponent {
+
+}
